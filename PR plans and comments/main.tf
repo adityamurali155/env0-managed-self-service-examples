f297e133c4a9406c30051dc9f86331a66f67add7
Aditya Murali <adityamurali2@gmail.com>
2024-07-01 14:47:53 +0530

    Update ACL

--- PR plans and comments/main.tf
+++ PR plans and comments/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "env0-self-service-bucket" {
   bucket = "env0-pr-plan-bucket"
-
+  acl = "public-read"
   tags = {
     Name        = "Pull request bucket"
     Environment = "Dev"
