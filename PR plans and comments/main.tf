a50bc6c739ac49b8d11622bfca16d8e49d3cdb65
adityamurali155 <adityamurali2@gmail.com>
2024-07-01 14:11:12 +0530

    edited s3 name

--- PR plans and comments/main.tf
+++ PR plans and comments/main.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "env0-pr-plan-bucket" {
-  bucket = "env0-PR-plan-bucket"
+resource "aws_s3_bucket" "env0-self-service-bucket" {
+  bucket = "env0-pr-plan-bucket"
 
   tags = {
     Name        = "Pull request bucket"
