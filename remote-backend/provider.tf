0ab12f8fcffd87d7063c69e38c0a315245aa4cdb
adityamurali155 <adityamurali2@gmail.com>
2024-07-02 12:16:32 +0530

    added remote backend

--- remote-backend/provider.tf
+++ remote-backend/provider.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.56.1"
+    }
+  }
   cloud {
     hostname     = "backend.api.env0.com"
     organization = "2d03541c-1a22-4e76-a10d-2b5e22584b7f.fbd80c90-78fe-46c7-a401-7ab161ec02cb"
@@ -6,4 +12,8 @@ terraform {
       name = "my-first-remote-apply"
     }
   }
+}
+
+provider "aws" {
+  region =  "us-east-1"
 }
