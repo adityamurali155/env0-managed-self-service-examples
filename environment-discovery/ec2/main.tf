dc517db294c3ef00c466fc5abd0136e8f686bc06
adityamurali155 <adityamurali2@gmail.com>
2024-07-02 19:36:14 +0530

    added ec2

---
+++ environment-discovery/ec2/main.tf
@@ -0,0 +1,25 @@
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+resource "aws_instance" "tg-example" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  subnet_id = data.aws_subnets.default.ids[0]
+
+  tags = {
+    Name = "TerragruntExampleEC2"
+  }
+
+}
+
+output "instance_id" {
+  value = aws_instance.tg-example.id
+}
