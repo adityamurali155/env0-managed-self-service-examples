ce66a8d1d1c3e583a2aa89a61b007b1ea72f13bb
adityamurali155 <adityamurali2@gmail.com>
2024-07-02 22:46:45 +0530

    modified custom flow

--- custom-flows/main.tf
+++ custom-flows/main.tf
@@ -1,69 +1,11 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_s3_bucket" "cf_bucket" {
+  bucket = "custom-flow-bucket-2312"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.allow_all_sg.id]
-  
-  tags = {
-    Name = "Example-instance"
-  }
-}
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
-  }
-}
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "main-subnet"
-  }
-}
-resource "aws_security_group" "allow_all_sg" {
-  name        = "allow_all_sg"
-  description = "Allow all inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "allow_all_sg"
-  }
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" 
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" 
-    cidr_blocks = ["0.0.0.0/0"]
+  tags {
+    Name = "Custom Flow Bucket"
   }
 }
 
-output "public-ip" {
-    value = aws_instance.web.public_ip
+output "bucket_name" {
+  value = aws_s3_bucket.cf_bucket.id
 }
