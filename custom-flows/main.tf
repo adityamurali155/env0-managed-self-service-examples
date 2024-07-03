82e9da0e31003287922b20c3fa6b8e8a920e305c
adityamurali155 <adityamurali2@gmail.com>
2024-07-03 13:17:22 +0530

    modified custom flows

--- custom-flows/main.tf
+++ custom-flows/main.tf
@@ -1,11 +1,24 @@
-resource "aws_s3_bucket" "cf_bucket" {
-  bucket = "custom-flow-bucket-2312"
-
-  tags = {
-    Name = "Custom Flow Bucket"
-  }
+data "aws_vpc" "default" {
+  default = true
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.cf_bucket.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+resource "aws_instance" "tg-example" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  subnet_id = data.aws_subnets.default.ids[0]
+
+  tags = {
+    Name = "TerragruntExampleEC2"
+  }
+
+}
+
+output "instance_id" {
+  value = aws_instance.tg-example.id
 }
